7235c9f50519e4172552f00615639241da5d4654
nguyenquanicd <nguyenquan.icd@gmail.com>
2020-04-30 12:17:45 +0700

    Add the flow chart of the RTL library read process

--- lib/rtlLib/RegGen_ApbIf.sv
+++ lib/rtlLib/RegGen_ApbIf.sv
@@ -1,15 +1,19 @@
 //---------------------------------------------------------------
 //Copyright © 2020 by VLSI Technology blog in Vietnam
 //Website    : http://nguyenquanicd.blogspot.com/
-//Author     : Nguyen Hung Quan (nguyenquan.icd@gmail.com)
-//Description: Register file library with APB interface
+//Author     :
+//  RTL design - Nguyen Hung Quan (nguyenquan.icd@gmail.com)
+//  Tool design- Le Hoang Van     (lehoangvan.for.business@gmail.com)
+//Description: Register file with APB interface
 //Language   : System Verilog
+//Model type : Synthesizable
 //---------------------------------------------------------------
 //Version    : 1.0
 //Date       : Apr.30.2020
 //---------------------------------------------------------------
 module $GenModuleName
   #(
+    parameter  int REGGEN_SYNC_STAGE = $GenSyncStageParam,
     parameter  int REGGEN_ADDR_WIDTH = $GenAddrParam,
     parameter  int REGGEN_DATA_WIDTH = $GenDataParam,
     localparam int REGGEN_STRB_WIDTH = REGGEN_DATA_WIDTH/8
@@ -32,15 +36,39 @@ module $GenModuleName
     output logic pslverr,
     output logic [REGGEN_DATA_WIDTH-1:0] prdata,
     //User interface is synchronized to reg_clk
-    input  logic write_protect_en,
-    input  logic $GenInternalWriteReg,
-    input  logic $GEnInternalWriteRegWe,
-    output logic [REGGEN_STRB_WIDTH-1:0] $GenRegName_byte_we,
-    output logic $GenRegName_$GenRegField_set,
-    output logic $GenRegName_$GenRegField_clr,
-    output logic [REGGEN_DATA_WIDTH-1:0] $GenRegName_$GenRegField_reg
+    $GenWProt input  logic write_protect_en,
+    $GenStartLoop
+    $RWI$RO$ROC$ROS input  logic $GenRegName_$GenRegField_ivalue,
+    $RWI$RO$ROC$ROS input  logic $GenRegName_$GenRegField_iwe,
+    $POW  output logic [REGGEN_STRB_WIDTH-1:0] $GenRegName_byte_we,
+    $POW1 output logic $GenRegName_$GenRegField_set,
+    $POW0 output logic $GenRegName_$GenRegField_clr,
+    $RW$RWI$RW_RC$RW_RS$RW_WC$RW_WS$RW_W1C$RW_W0S$RW_W1S$RW_W0S$WO$WOC$WOS$WO0$WO1 output logic [REGGEN_DATA_WIDTH-1:0] $GenRegName_$GenRegField_reg
+    $GenEndLoop
   );
   //pclk
-  
+  assign setup_phase = psel & ~penable;
+  //Synchronizer
+  $GenStartBlock$GenAsync
+    //Access request
+    assign req_inv = setup_phase;
+    $GenAsyncReset always_ff @ (posedge pclk, negedge presetn) begin
+    $GenSyncReset always_ff @ (posedge pclk) begin
+      if (!presetn)
+        req_in <= '0;
+      else if (req_inv)
+        reg_in <= ~req_in;
+    end
+    $GenAsyncReset always_ff @ (posedge reg_clk, negedge reg_rst_n) begin
+    $GenSyncReset always_ff @ (posedge reg_clk) begin
+      if (!reg_rst_n)
+        req_sync <= '0;
+      else
+        req_sync <= {req_sync[REGGEN_SYNC_STAGE-1:1], req_in};
+    end
+    assign req_en = req_sync[1]
+    //Access acknowledge
+    
+  $GenEndBlock$GenAsync
   //
 endmodule: $GenModuleName
